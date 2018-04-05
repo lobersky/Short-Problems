dd564617b7095c06d75ccea1605b930d61e2720c
lobersky <l.obersky@gmail.com>
2018-04-05 16:32:40 +1000

    Continue-Run-Issue

--- PalindromeChecker/PalindromeChecker/Program.cs
+++ PalindromeChecker/PalindromeChecker/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Text.RegularExpressions;
+using System.Diagnostics;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,22 +12,40 @@ namespace PalindromeChecker
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Check if your string is a palindrome:");
-            string userInputString = Console.ReadLine();
+            Process myProcess = null;
 
-            string checkString = ReverseString(userInputString);
-
-            if (userInputString == checkString)
+            try
             {
-                Console.WriteLine("The string is a palindrome");
-            }
-            else
-            {
-                Console.WriteLine("The string is not a palindrome");
+                myProcess = Process.Start("PalindromeChecker.exe");
+
+                while (!myProcess.HasExited)
+                {
+                    Console.WriteLine("Check if your string is a palindrome:");
+                    string userInputString = Console.ReadLine();
+
+                    userInputString = Regex.Replace(userInputString, @"\s", "");
+                    string checkString = ReverseString(userInputString);
+
+                    if (userInputString == checkString)
+                    {
+                        Console.WriteLine("The string is a palindrome");
+                    }
+                    else
+                    {
+                        Console.WriteLine("The string is not a palindrome");
+                    }
+                    Console.ReadLine();
+                }
             }
 
-            Console.ReadLine();
-        }
+            finally
+            {
+                if (myProcess != null)
+                {
+                    myProcess.Close();
+                }
+            }
+         }
 
         // Helper method - String Reverser
         public static string ReverseString(string s)
